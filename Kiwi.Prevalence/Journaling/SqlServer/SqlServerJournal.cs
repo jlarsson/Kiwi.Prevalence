deb952093a28b08a3f592795f3444a6b3fee4658
Joakim Larsson <mimmertim@gmail.com>
2012-09-13 12:54:32 +0200

    Changed naming convention for database tables

--- Kiwi.Prevalence/Journaling/SqlServer/SqlServerJournal.cs
+++ Kiwi.Prevalence/Journaling/SqlServer/SqlServerJournal.cs
@@ -180,8 +180,8 @@ namespace Kiwi.Prevalence.Journaling.SqlServer
                 RepositoryName = repositoryName;
 
                 CreateJournalTableCommand = FormatCommand(
-                    @"IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Journal{0}') AND type in (N'U'))
-	CREATE TABLE [Journal{0}](
+                    @"IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'{0}Journal') AND type in (N'U'))
+	CREATE TABLE [{0}Journal](
 		[Revision] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
         [Type] nvarchar(MAX) NOT NULL,
 		[CommandJson] [ntext] NOT NULL,
@@ -189,24 +189,24 @@ namespace Kiwi.Prevalence.Journaling.SqlServer
 	)");
 
                 CreateSnapshotTableCommand = FormatCommand(
-                    @"IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'Snapshot{0}') AND type in (N'U'))
-	CREATE TABLE [Snapshot{0}](
+                    @"IF  NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'{0}Snapshot') AND type in (N'U'))
+	CREATE TABLE [{0}Snapshot](
 		[Revision] [int] NOT NULL PRIMARY KEY,
 		[ModelJson] [ntext] NOT NULL,
         [Timestamp] [timestamp] NOT NULL
 	)");
-                LogCommand = FormatCommand(@"INSERT INTO [Journal{0}] (Type, CommandJson) VALUES (@Type, @CommandJson)");
+                LogCommand = FormatCommand(@"INSERT INTO [{0}Journal] (Type, CommandJson) VALUES (@Type, @CommandJson)");
                 SaveSnapshot =
                     FormatCommand(
-                        @"INSERT INTO [Snapshot{0}] (Revision, ModelJson) SELECT TOP 1 Revision,@ModelJson FROM [Journal{0}] ORDER BY Revision DESC");
+                        @"INSERT INTO [{0}Snapshot] (Revision, ModelJson) SELECT TOP 1 Revision,@ModelJson FROM [{0}Journal] ORDER BY Revision DESC");
 
-                LastSnapshot = FormatCommand(@"SELECT Revision, ModelJson FROM [Snapshot{0}] ORDER BY Revision DESC");
+                LastSnapshot = FormatCommand(@"SELECT Revision, ModelJson FROM [{0}Snapshot] ORDER BY Revision DESC");
                 GetCommandsFromRevision =
                     FormatCommand(
-                        "SELECT Type, CommandJson FROM [Journal{0}] WHERE Revision > @Revision ORDER BY Revision");
+                        "SELECT Type, CommandJson FROM [{0}Journal] WHERE Revision > @Revision ORDER BY Revision");
 
-                PurgeJournal = FormatCommand("delete [Journal{0}]");
-                PurgeSnapshot = FormatCommand("delete [Snapshot{0}]");
+                PurgeJournal = FormatCommand("delete [{0}Journal]");
+                PurgeSnapshot = FormatCommand("delete [{0}Snapshot]");
             }
 
             public string PurgeSnapshot { get; private set; }
@@ -228,7 +228,7 @@ namespace Kiwi.Prevalence.Journaling.SqlServer
 
             private string FormatCommand(string sql)
             {
-                return string.Format(sql, (string.IsNullOrEmpty(RepositoryName) ? "" : "_" + RepositoryName));
+                return string.Format(sql, RepositoryName ?? "");
             }
         }
 
